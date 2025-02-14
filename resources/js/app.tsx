9ccb436dd79d57ff30360b24252fb6f0ac718673
DanielAlexandrow <didkoaleksandrov@gmail.com>
2025-02-14 19:29:25 +0200

    Update app.tsx

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,24 +1,35 @@
 import './bootstrap';
 import '../css/app.css';
+
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import 'primeflex/primeflex.css';
+
+import '../css/layout.css'
+
+
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
+import {LayoutProvider} from "@/Layouts/layout/context/layoutcontext.jsx";
+import {PrimeReactProvider} from "primereact/api";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-const queryClient = new QueryClient();
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
         root.render(
-            <QueryClientProvider client={queryClient}>
-                <App {...props} />
-            </QueryClientProvider>
+            <PrimeReactProvider>
+            <LayoutProvider>
+            <App {...props} />
+            </LayoutProvider>
+            </PrimeReactProvider>
         );
     },
     progress: {
