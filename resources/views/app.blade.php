a71a7708fa1fc16c348c9d33374cdf788e695309
DanielAlexandrow <didkoaleksandrov@gmail.com>
2025-02-14 19:20:03 +0200

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,53 +1,26 @@
 <!DOCTYPE html>
-<html class='dark scroll-smooth' lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'LaraReact') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @routes
         @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
 
-        <script>
-            let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-
-            updateMode()
-            darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
-            window.addEventListener('storage', updateModeWithoutTransitions)
-
-            function updateMode() {
-                let isSystemDarkMode = darkModeMediaQuery.matches
-                let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
-
-                if (isDarkMode) {
-                    document.documentElement.classList.add('dark')
-                } else {
-                    document.documentElement.classList.remove('dark')
-                }
-
-                if (isDarkMode === isSystemDarkMode) {
-                    delete window.localStorage.isDarkMode
-                }
-            }
-
-            function disableTransitionsTemporarily() {
-                document.documentElement.classList.add('[&_*]:!transition-none')
-                window.setTimeout(() => {
-                    document.documentElement.classList.remove('[&_*]:!transition-none')
-                }, 0)
-            }
-
-            function updateModeWithoutTransitions() {
-                disableTransitionsTemporarily()
-                updateMode()
-            }
-        </script>
+        <link id="theme-css" href={{asset('/themes/tailwind-light/theme.css')}} rel="stylesheet"></link>
     </head>
-    <body class="font-sans text-[.915rem] tracking-tight antialiased">
+    <body class="font-sans antialiased">
         @inertia
     </body>
 </html>
+
+
