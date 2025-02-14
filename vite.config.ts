d5dd954e49b82f5e6a29b59c479dfb748453b898
DanielAlexandrow <didkoaleksandrov@gmail.com>
2025-02-14 19:21:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
 
 export default defineConfig({
-	plugins: [
-		laravel({
-			input: ['resources/css/app.css', 'resources/js/app.tsx'],
-			refresh: true,
-		}),
-		react(),
-		tsconfigPaths(),
-	],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './resources/js'),
-			'@/types': path.resolve(__dirname, './resources/js/types'),
-		},
-	},
-	server: {
-		host: '0.0.0.0',
-		hmr: {
-			host: 'localhost',
-		},
-		watch: {
-			usePolling: true,
-		},
-	},
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: ['resources/js/tests/setup.ts'],
-		coverage: {
-			provider: 'v8',
-			reporter: ['text', 'html'],
-			exclude: [
-				'node_modules/',
-				'tests/setup.ts',
-			],
-		},
-	},
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
 });
